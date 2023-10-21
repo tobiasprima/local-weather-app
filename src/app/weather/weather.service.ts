7cf41342ead9296289795ee0619fe18bf7aab1ce
Tobias Prima <primatjahja@gmail.com>
2023-10-21 19:46:01 +0800

    Add current weather data interfaces

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,6 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+interface ICurrentWeatherData {
+  weather: [{
+    description: string,
+    icon: string
+  }],
+  main: {
+    temp: number
+  },
+  sys: {
+    country: string
+  },
+  dt: number,
+  name: string
+}
 @Injectable({
   providedIn: 'root'
 })
