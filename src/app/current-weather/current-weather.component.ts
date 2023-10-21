3673123be746e11d9bd146f3421e30260d6fb26b
Tobias Prima <primatjahja@gmail.com>
2023-10-21 19:19:56 +0800

    Add Interfaces

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,4 +1,5 @@
 import { Component , OnInit } from '@angular/core';
+import { ICurrentWeather } from '../interfaces';
 
 @Component({
   selector: 'app-current-weather',
@@ -6,7 +7,17 @@ import { Component , OnInit } from '@angular/core';
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
-  constructor() {}
+  current: ICurrentWeather
+  constructor() {
+    this.current = {
+      city: 'Bethesda',
+      country: 'US',
+      date: new Date(),
+      image: 'assets/img/sunny.svg',
+      temperature: 72,
+      description: 'sunny',
+    } as ICurrentWeather
+  }
   ngOnInit(): void {
     
   }
